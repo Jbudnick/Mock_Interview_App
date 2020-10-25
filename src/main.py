63a59197be11189849aba03449c61b39637e247f
Jbud <Jbudn90@yahoo.com>
2020-10-24 19:52:42 -0600

    App now draws random question and generates them properly. Now need to be able to separate them by category

--- src/main.py
+++ src/main.py
@@ -1,2 +1,13 @@
 import numpy as np
+import pandas as pd
+import random
 
+questions_df = pd.read_csv('questions.csv', delimiter='\n', names=['Question'])
+
+questions = questions_df['Question'].values
+questions = [question for question in questions if not question.startswith('#')]
+
+Interview_questions = random.sample(set(questions), 5)
+
+for each in Interview_questions:
+    print(each)

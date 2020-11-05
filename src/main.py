d5c5d90ee5ef9dc039ee8aa45ab33f4b893f82bd
Jbud <Jbudn90@yahoo.com>
2020-11-04 19:06:44 -0700

    Converted dataset into pandas dataframe so that questions can be arranged by category

--- src/main.py
+++ src/main.py
@@ -2,12 +2,27 @@ import numpy as np
 import pandas as pd
 import random
 
-questions_df = pd.read_csv('questions.csv', delimiter='\n', names=['Question'])
+num_questions = 5
 
-questions = questions_df['Question'].values
-questions = [question for question in questions if not question.startswith('#')]
+Interview_df = pd.DataFrame(columns = ['Question', 'Category'])
+current_category = ''
+line_num = 0
 
-Interview_questions = random.sample(set(questions), 5)
+fhand = open('questions.csv')
+for line in fhand:
+    if line.startswith('#'):
+        current_category = line.replace('#', '').replace('\n', '')
+    else:
+        Interview_df.loc[line_num] = [line.replace('\n', ''), current_category]
+        line_num += 1
 
-for each in Interview_questions:
+#Add category designation function here
+
+questions = Interview_df['Question'].values
+
+Interview_questions = random.sample(set(questions), num_questions)
+
+for i, each in enumerate(Interview_questions):
+    print('\n')
     print(each)
+    input('\n\nEnter any key for next question ({} Remaining): '.format(num_questions - 1 - i))

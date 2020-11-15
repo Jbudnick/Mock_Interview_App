34a0c71315de3bc02922d90c96cec172c60d739f
Jbud <Jbudn90@yahoo.com>
2020-11-14 21:01:25 -0700

    Added toggled questions, and images. App is complete and ready to be deployed on AWS for public use.

--- src/main.py
+++ src/main.py
@@ -3,15 +3,28 @@ import pandas as pd
 import random
 from os import system
 
-current_category = ''
-line_num = 0
+def import_question_db():
+    line_num = 0
+    current_category = ''
+    Interview_df = pd.DataFrame(columns=['Question', 'Category'])
+    fhand = open('questions.csv')
+    for line in fhand:
+        if line == '\n':
+            continue
+        if line.startswith('#'):
+            current_category = line.replace('#', '').replace('\n', '')
+        else:
+            Interview_df.loc[line_num] = [line.replace('\n', ''), current_category]
+            line_num += 1
+    return Interview_df
 
-def select_mode(df):
-    system('clear')
-    print('\nSelect a mode: \n 1. All Questions (Data Science + Behavioral) \n 2. Behavioral Only \n 3. Behavioral + Tech (except Data Science) \n 4. Data Science/Tech Only ')
-    mode_select = None
-    while mode_select not in (1, 2, 3, 4):
-        mode_select = int(input('Select Mode 1, 2, 3, or 4: '))
+def select_mode(df, mode_select = None):
+    if mode_select == None:
+        system('clear')
+        print('\nSelect a mode: \n 1. All Questions (Data Science + Behavioral) \n 2. Behavioral Only \n 3. Behavioral + Tech (except Data Science) \n 4. Data Science/Tech Only ')
+        mode_select = None
+        while mode_select not in (1, 2, 3, 4):
+            mode_select = int(input('Select Mode 1, 2, 3, or 4: '))
     if mode_select == 1:
         new_df = df
     elif mode_select == 2:
@@ -24,69 +37,56 @@ def select_mode(df):
         new_df = df[df['Category'] != 'Behavioral']
     return new_df
 
-Interview_df = pd.DataFrame(columns=['Question', 'Category'])
-
-fhand = open('questions.csv')
-for line in fhand:
-    if line == '\n':
-        continue
-    if line.startswith('#'):
-        current_category = line.replace('#', '').replace('\n', '')
+def options_select():
+    options = ''
+    while options.lower() not in ('y', 'n'):
+        options = input('Use default options? Y/N: ')
+    if options.lower() == 'y':
+        num_questions = 5
+        follow_up_prob = 0
     else:
-        Interview_df.loc[line_num] = [line.replace('\n', ''), current_category]
-        line_num += 1
+        num_questions = int(input('Enter number of questions: '))
+        follow_up_prob = float(input(
+            '\nNote: Some follow ups may not make sense\nEnter follow up question probability (0 to 1): '))
+        if follow_up_prob > 1:
+            follow_up_prob = follow_up_prob/100
+            input('Greater than 1 detected. Converted to probability {}. Enter any key to continue: '.format(
+                follow_up_prob))
+    return num_questions, follow_up_prob
 
-probe_q = Interview_df[Interview_df['Category'] == 'PROBES']['Question'].values
-Interview_df = select_mode(Interview_df)
-questions = Interview_df[(Interview_df['Category'] != 'PROBES') & 
-                         (Interview_df['Category'] != 'Introduction')]['Question'].values
-
-options = ''
-while options.lower() not in ('y', 'n'):
-    options = input('Use default options? Y/N: ')
-if options.lower() == 'y':
-    num_questions = 5
-    follow_up_prob = 0
-else:
-    num_questions = int(input('Enter number of questions: '))
-    follow_up_prob = float(input('Note: Some follow ups may not make sense\nEnter follow up question probability (0 to 1): '))
-    if follow_up_prob > 1:
-        follow_up_prob = follow_up_prob/100
-        input('Greater than 1 detected. Converted to probability {}. Enter any key to continue: '.format(follow_up_prob))
-
-Interview_questions = random.sample(set(questions), num_questions - 1)
-question_list = []
-
-system('clear')
-# Intro question
-print('\n---------------------------------------------------------------------')
-print('\n')
-first_question = np.random.choice(
-    Interview_df[Interview_df['Category'] == 'Introduction']['Question'].values)
-question_list.append(first_question)
-print(first_question)
-print('\n\n---------------------------------------------------------------------')
-num_questions -= 1
-next = input('\n\nEnter any key for next question ({} Remaining): '.format(num_questions))
-system('clear')
-
-for i, question in enumerate(Interview_questions):
-    print('\n---------------------------------------------------------------------')
-    print('\n')
-    question_list.append(question)
-    print(question)
-    probe = np.random.rand(1)[0]
-    if probe <= follow_up_prob:
-        follow_up = np.random.choice(probe_q)
-        question_list.append(follow_up)
-        print(follow_up)
-    print('\n\n---------------------------------------------------------------------')
-    next = input('\n\nEnter any key for next question ({} Remaining): '.format(num_questions - 1 - i))
+def ask_questions(Interview_df, num_questions, question_list = []):
     system('clear')
-    if next in ('quit','q', 'exit'):
-        break
+    first_question = np.random.choice(
+        Interview_df[Interview_df['Category'] == 'Introduction']['Question'].values)
+    questions = Interview_df[(Interview_df['Category'] != 'PROBES') &
+                             (Interview_df['Category'] != 'Introduction')]['Question'].values
+    Interview_questions = [first_question]
+    Interview_questions.extend(random.sample(set(questions), num_questions - 1))
+    for i, question in enumerate(Interview_questions):
+        print('\n---------------------------------------------------------------------\n\n')
+        question_list.append(question)
+        print(question)
+        probe = np.random.rand(1)[0]
+        if len(question_list) > 1 and probe <= follow_up_prob:
+            follow_up = np.random.choice(probe_q)
+            question_list.append(follow_up)
+            print(follow_up)
+        print('\n\n---------------------------------------------------------------------')
+        num_questions -= 1
+        next = input('\n\nEnter any key for next question ({} Remaining): '.format(num_questions))
+        system('clear')
+        if next in ('quit', 'q', 'exit'):
+            break
+    return question_list
 
-print("Questions Asked:\n---------------------------\n")
-for q in question_list:
-    print(q)
-print('\n---------------------------')
+if __name__ == '__main__':
+    Interview_df = import_question_db()
+    probe_q = Interview_df[Interview_df['Category'] == 'PROBES']['Question'].values
+    Interview_df = select_mode(Interview_df)
+    num_questions, follow_up_prob = options_select()
+    question_list = ask_questions(Interview_df, num_questions)
+
+    print("\nQuestions Asked:\n---------------------------\n")
+    for q in question_list:
+        print(q)
+    print('\n---------------------------')
---
+++ web_app/app.py
@@ -0,0 +1,89 @@
+import flask
+import sys
+import numpy as np
+import pandas as pd
+import random
+
+sys.path.append("..")
+from src import main
+
+APP = flask.Flask(__name__)
+@APP.route('/')
+def get_input():
+    return '''
+        <style>
+        h1 {
+            font-size: 35px;
+            text-align: center;
+        }
+        header {
+            text-align: center;
+        }
+        form {
+            font-size: 20px;
+            text-align: center;
+        }
+        select {
+            font-size: 15px;
+        }
+        input {
+            font-size: 18px;
+        }
+        footer {
+            padding: 5%;
+        }
+        </style>
+        <header><img src="https://blog.gurock.com/wp-content/uploads/2015/08/interview.png" style="height:200px"></header>
+        <h1>Mock Interview Question Generator</h1>
+        <form action="/interview" method = 'POST'>
+        <label for="mode">Choose Interview Mode:</label><br>
+        <select name="mode" id="mode">
+            <option value="1">Behavioral and Data Science</option>
+            <option value="2">Behavioral Only</option>
+            <option value="3">Behavioral and Tech (No Data Science)</option>
+            <option value="4">Data Science Only (No Behavioral)</option>
+        </select>
+        <br><br>
+        <label for="num_questions">Select Number of Questions</label><br>
+        <select name="num_questions" id="num_questions">
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+        </select>
+        <br><br>
+        <input type="submit" value="Start Mock Interview">
+        </form>
+        <br><br><br><br><br><br>
+        <footer>Created by Jacob Budnick<br>
+        Connect with me:<br>
+        <a href="https://www.linkedin.com/in/jacobbudnick/">
+        <img src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/linkedin.svg" alt="LinkedInJbudnick" style="width:30px;"></a>&nbsp;
+        <a href="https://github.com/Jbudnick/">
+        <img src="https://cdn.jsdelivr.net/npm/simple-icons@3.12.3/icons/github.svg" alt="GithubJbudnick" style="width:30px;"></a>&nbsp;
+        <a href="mailto:Jbudnick5@gmail.com">
+        <img src="https://cdn.jsdelivr.net/npm/simple-icons@3.5.0/icons/gmail.svg" alt="Jbudnick5@gmail.com" style="width:30px;"></a>
+        </footer>
+        '''
+
+
+
+@APP.route('/interview', methods = ["GET","POST"])
+def interview():
+    mode = flask.request.form['mode']
+    num_questions = int(flask.request.form['num_questions'])
+    Interview_df = main.import_question_db()
+    Interview_df = main.select_mode(Interview_df, mode_select= int(mode))
+    question_list = []
+    first_question = np.random.choice(
+        Interview_df[Interview_df['Category'] == 'Introduction']['Question'].values)
+    probe_q = Interview_df[Interview_df['Category']
+                        == 'PROBES']['Question'].values
+    questions = Interview_df[(Interview_df['Category'] != 'PROBES') &
+                            (Interview_df['Category'] != 'Introduction')]['Question'].values
+    Interview_questions = [first_question]
+    Interview_questions.extend(random.sample(set(questions), num_questions - 1))
+    return flask.render_template("index.html", len = len(Interview_questions), Interview_questions = Interview_questions)
+
+if __name__ == '__main__':
+    APP.run(host = '0.0.0.0', port = 8080, debug = True)
